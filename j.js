e28841cf8aaaf068308c11a61e0fcf592986107e
мир <106350647+mir3133031@users.noreply.github.com>
2022-06-17 14:41:43 +0500

    Add files via upload

---
+++ j.js
@@ -0,0 +1,27 @@
+let arr = [4, 16, 19, 22, 11, 144, 967, 19124]
+for (let i = 0; i < arr.length; i++)
+{
+  if ((arr[i] % 2) === 0)
+  {
+  	
+let odd = [arr[i]]
+console.log(odd);
+
+
+  }
+}
+
+let even = arr.filter(num => num % 2);
+console.log(even);
+
+
+
+
+
+
+
+
+
+
+// console.log(arr[i]);
+
